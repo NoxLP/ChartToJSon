5af9cd3c1da745e36bf5a2bb5a311609437d3422
NoxLP <nox_nox_@hotmail.com>
2020-02-17 17:37:09 +0000

    Remove old plugin directory

--- StandardChartEntityTemplatesPlugin/Model/Rewards/INodeReward.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StoryChart.Model.Rewards
-{
-    public interface INodeReward
-    {
-        string Description { get; set; }
-        string Id { get; set; }
-        RewardsTypesEnum Type { get; set; }
-    }
-}
